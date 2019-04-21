9a67517915505c9fc5285ce3bedef9d2bce28c4e
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2019-04-22 00:31:33 +0200

    v1.61: Fix final modifier removal still not working on Java 12

--- src/main/java/com/bergerkiller/mountiplex/reflection/util/DisableFinalModifierHelper.java
+++ src/main/java/com/bergerkiller/mountiplex/reflection/util/DisableFinalModifierHelper.java
@@ -1,6 +1,5 @@
 package com.bergerkiller.mountiplex.reflection.util;
 
-import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
@@ -12,11 +11,22 @@ public class DisableFinalModifierHelper {
     private static final RemoveFinalModifierMethod REMOVE_FINAL_MODIFIER_METHOD;
 
     static {
-        RemoveFinalModifierMethod method = new RemoveFinalModifierUsingReflection();
-        if (!method.valid()) {
-            method = new RemoveFinalModifierUsingConstructorHack();
+        RemoveFinalModifierMethod method = null;
+        if (method == null) {
+            try {
+                method = new RemoveFinalModifierUsingReflection();
+            } catch (Throwable t) {
+                // t.printStackTrace();
+            }
         }
-        if (!method.valid()) {
+        if (method == null) {
+            try {
+                method = new RemoveFinalModifierUsingUnsafe();
+            } catch (Throwable t) {
+                // t.printStackTrace();
+            }
+        }
+        if (method == null) {
             method = new RemoveFinalModifierUnsupported();
         }
         REMOVE_FINAL_MODIFIER_METHOD = method;
@@ -26,100 +36,50 @@ public class DisableFinalModifierHelper {
         REMOVE_FINAL_MODIFIER_METHOD.remove(field);
     }
 
-    // Required for Java 12
-    private static class RemoveFinalModifierUsingConstructorHack implements RemoveFinalModifierMethod {
-        private final Object reflectionFactory;
-        private final java.lang.reflect.Method newFieldAccessorMethod;
-        private final java.lang.reflect.Method setFieldAccessorMethod;
-        private final java.lang.reflect.Field signatureField;
-        private final java.lang.reflect.Field annotationsField;
-        private final java.lang.reflect.Field slotField;
-        private final java.lang.reflect.Constructor<?> fieldConstr;
+    // Required for Java 12. (Ab)uses Unsafe to get around the permission manager to do this.
+    private static class RemoveFinalModifierUsingUnsafe implements RemoveFinalModifierMethod {
+        private final Object unsafe;
+        private final java.lang.reflect.Method _setFieldAccessorMethod;
 
-        public RemoveFinalModifierUsingConstructorHack() {
-            Object ref_o = null;
-            java.lang.reflect.Method newfa_m = null,
-                                     setfa_m = null;
-            java.lang.reflect.Field sig_f = null,
-                                    ann_f = null,
-                                    slt_f = null;
-            java.lang.reflect.Constructor<?> fld_c = null;
-            try {
-                // Retrieve ReflectionFactory instance
-                Field refFactoryField = AccessibleObject.class.getDeclaredField("reflectionFactory");
-                refFactoryField.setAccessible(true);
-                ref_o = refFactoryField.get(null);
-                refFactoryField.setAccessible(false);
+        public RemoveFinalModifierUsingUnsafe() throws Throwable {
+            Class<?> unsafeType = Class.forName("sun.misc.Unsafe");
+            Field f = unsafeType.getDeclaredField("theUnsafe");
+            f.setAccessible(true);
+            this.unsafe = f.get(null);
 
-                // Obtain the newFieldAccessor method
-                newfa_m = ref_o.getClass().getDeclaredMethod("newFieldAccessor", 
-                        java.lang.reflect.Field.class, boolean.class);
-                newfa_m.setAccessible(true);
-
-                // Obtain the Field setFieldAccessor method
-                setfa_m = java.lang.reflect.Field.class.getDeclaredMethod("setFieldAccessor",
-                        Class.forName("sun.reflect.FieldAccessor"), boolean.class);
-                setfa_m.setAccessible(true);
-
-                // A bunch of fields and the main Field constructor we need
-                sig_f = java.lang.reflect.Field.class.getDeclaredField("signature");
-                ann_f = java.lang.reflect.Field.class.getDeclaredField("annotations");
-                slt_f = java.lang.reflect.Field.class.getDeclaredField("slot");
-                fld_c = java.lang.reflect.Field.class.getDeclaredConstructor(
-                        Class.class, String.class, Class.class,
-                        int.class, int.class, String.class, byte[].class);
-                sig_f.setAccessible(true);
-                ann_f.setAccessible(true);
-                slt_f.setAccessible(true);
-                fld_c.setAccessible(true);
-            } catch (Throwable t) {
-                // t.printStackTrace();
-            }
-            this.reflectionFactory = ref_o;
-            this.newFieldAccessorMethod = newfa_m;
-            this.setFieldAccessorMethod = setfa_m;
-            this.signatureField = sig_f;
-            this.annotationsField = ann_f;
-            this.slotField = slt_f;
-            this.fieldConstr = fld_c;
+            java.lang.reflect.Method acquire = java.lang.reflect.Field.class.getDeclaredMethod("acquireFieldAccessor", boolean.class);
+            this._setFieldAccessorMethod = java.lang.reflect.Field.class.getDeclaredMethod("setFieldAccessor",
+                    acquire.getReturnType(), boolean.class);
         }
 
         @Override
+        @SuppressWarnings("restriction")
         public void remove(Field field) throws IllegalAccessException {
-            if ((field.getModifiers() & Modifier.FINAL) == 0) {
-                return;
-            }
+            // Set cached FieldAccessor object to null to force a re-initialization of it
+            // This way, if someone else used reflection to get/set before, it won't fail
             try {
-                // Create a copy of the Field object with FINAL removed from modifiers
-                Field fieldWithChangedModifiers = (Field) this.fieldConstr.newInstance(
-                        field.getDeclaringClass(),
-                        field.getName(),
-                        field.getType(),
-                        field.getModifiers() & ~Modifier.FINAL,
-                        this.slotField.get(field),
-                        this.signatureField.get(field),
-                        this.annotationsField.get(field));
-                
-                // Use this field object to create a field accessor
-                Object fieldAccessor = this.newFieldAccessorMethod.invoke(this.reflectionFactory,
-                        fieldWithChangedModifiers, true);
-
-                // Apply the field accessor to the input field, which will now use it instead
-                this.setFieldAccessorMethod.invoke(field, fieldAccessor, true);
+                this._setFieldAccessorMethod.setAccessible(true);
+                this._setFieldAccessorMethod.invoke(field, null, true);
+                this._setFieldAccessorMethod.setAccessible(false);
             } catch (Throwable t) {
-                throw new IllegalAccessException("Failed to disable final modifier");
+                throw new IllegalAccessException("Failed to clean up previous field accessor");
             }
-        }
 
-        @Override
-        public boolean valid() {
-            return this.reflectionFactory != null &&
-                   this.newFieldAccessorMethod != null &&
-                   this.setFieldAccessorMethod != null &&
-                   this.signatureField != null &&
-                   this.slotField != null &&
-                   this.annotationsField != null &&
-                   this.fieldConstr != null;
+            // Switch modifier field to without final
+            final long field_offset = 24L;
+            sun.misc.Unsafe u = (sun.misc.Unsafe) unsafe;
+            int old_value = u.getInt(field, field_offset);
+            if (old_value != field.getModifiers()) {
+                throw new IllegalAccessException("Expected old modifier field value is incorrect");
+            }
+            int new_value = old_value & ~Modifier.FINAL;
+            if (old_value != new_value) {
+                u.putInt(field, field_offset, new_value);
+                if (field.getModifiers() != new_value) {
+                    u.putInt(field, field_offset, old_value);
+                    throw new IllegalAccessException("Expected new modifier field value is incorrect");
+                }
+            }
         }
     }
 
@@ -128,16 +88,11 @@ public class DisableFinalModifierHelper {
         private final java.lang.reflect.Field _modifiersField;
         private final java.lang.reflect.Method _setFieldAccessorMethod;
 
-        public RemoveFinalModifierUsingReflection() {
-            java.lang.reflect.Field mod_f = null;
-            java.lang.reflect.Method setfa_m = null;
-            try {
-                mod_f = java.lang.reflect.Field.class.getDeclaredField("modifiers");
-                setfa_m = java.lang.reflect.Field.class.getDeclaredMethod("setFieldAccessor",
-                        Class.forName("sun.reflect.FieldAccessor"), boolean.class);
-            } catch (Throwable t) {}        
-            this._modifiersField = mod_f;
-            this._setFieldAccessorMethod = setfa_m;
+        public RemoveFinalModifierUsingReflection() throws Throwable {
+            this._modifiersField = java.lang.reflect.Field.class.getDeclaredField("modifiers");
+            java.lang.reflect.Method acquire = java.lang.reflect.Field.class.getDeclaredMethod("acquireFieldAccessor", boolean.class);
+            this._setFieldAccessorMethod = java.lang.reflect.Field.class.getDeclaredMethod("setFieldAccessor",
+                    acquire.getReturnType(), boolean.class);
         }
 
         @Override
@@ -157,27 +112,111 @@ public class DisableFinalModifierHelper {
             this._modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
             this._modifiersField.setAccessible(false);
         }
-
-        @Override
-        public boolean valid() {
-            return this._modifiersField != null && this._setFieldAccessorMethod != null;
-        }
     }
 
+    // This old code doesn't work on Java 12 because the 'slot' (and annotations) field is not accessible
+    // I could find no way to retrieve this value without using Unsafe
+    /*
+    // Required for Java 12
+    private static class RemoveFinalModifierUsingConstructorHack implements RemoveFinalModifierMethod {
+        //private final Object reflectionFactory;
+        //private final java.lang.reflect.Method newFieldAccessorMethod;
+        private final java.lang.reflect.Method acquireFieldAccessorMethod;
+        private final java.lang.reflect.Method setFieldAccessorMethod;
+        private final java.lang.reflect.Method getGenericSignatureMethod;
+        //private final java.lang.reflect.Field slotField;
+        private final java.lang.reflect.Constructor<?> fieldConstr;
+
+        public RemoveFinalModifierUsingConstructorHack() {
+            //Object ref_o = null;
+            java.lang.reflect.Method acqfa_m = null,
+                                     setfa_m = null,
+                                     getsig_m = null;
+            java.lang.reflect.Field slt_f = null;
+            java.lang.reflect.Constructor<?> fld_c = null;
+            try {
+                // Retrieve ReflectionFactory instance
+ 
+                //Field refFactoryField = AccessibleObject.class.getDeclaredField("reflectionFactory");
+               // refFactoryField.setAccessible(true);
+                //ref_o = refFactoryField.get(null);
+               // refFactoryField.setAccessible(false);
+
+                // Obtain the newFieldAccessor method
+                //newfa_m = ref_o.getClass().getDeclaredMethod("newFieldAccessor", 
+                //        java.lang.reflect.Field.class, boolean.class);
+                //newfa_m.setAccessible(true);
+
+                // Obtain the Field method
+                acqfa_m = java.lang.reflect.Field.class.getDeclaredMethod("acquireFieldAccessor",
+                        boolean.class);
+                acqfa_m.setAccessible(true);
+
+                // Obtain the Field setFieldAccessor method
+                setfa_m = java.lang.reflect.Field.class.getDeclaredMethod("setFieldAccessor",
+                        acqfa_m.getReturnType(), boolean.class);
+                setfa_m.setAccessible(true);
+
+                // Obtain the Field getGenericSignature method
+                getsig_m = java.lang.reflect.Field.class.getDeclaredMethod("getGenericSignature");
+                getsig_m.setAccessible(true);
+
+                // A bunch of fields and the main Field constructor we need
+                //slt_f = java.lang.reflect.Field.class.getDeclaredField("slot");
+                fld_c = java.lang.reflect.Field.class.getDeclaredConstructor(
+                        Class.class, String.class, Class.class,
+                        int.class, int.class, String.class, byte[].class);
+                fld_c.setAccessible(true);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+            this.acquireFieldAccessorMethod = acqfa_m;
+            this.setFieldAccessorMethod = setfa_m;
+            this.getGenericSignatureMethod = getsig_m;
+            //this.slotField = slt_f;
+            this.fieldConstr = fld_c;
+        }
+
+        @Override
+        public void remove(Field field) throws IllegalAccessException {
+            if ((field.getModifiers() & Modifier.FINAL) == 0) {
+                return;
+            }
+            try {
+                // Create a copy of the Field object with FINAL removed from modifiers
+                Field fieldWithChangedModifiers = (Field) this.fieldConstr.newInstance(
+                        field.getDeclaringClass(),
+                        field.getName(),
+                        field.getType(),
+                        field.getModifiers() & ~Modifier.FINAL,
+                        0,
+                        this.getGenericSignatureMethod.invoke(field),
+                        new byte[0]);
+
+                //System.out.println("SLOT " + this.slotField.get(field));
+
+                // Acquire field accessor using the custom modified Field
+                Object fieldAccessor = this.acquireFieldAccessorMethod.invoke(fieldWithChangedModifiers, true);
+                //UnsafeStaticObjectFieldAccessorImpl
+                System.out.println("CLAAAS: " + fieldAccessor.getClass());
+
+                // Apply the field accessor to the input field, which will now use it instead
+                this.setFieldAccessorMethod.invoke(field, fieldAccessor, true);
+            } catch (Throwable t) {
+                throw new IllegalAccessException("Failed to disable final modifier");
+            }
+        }
+    }
+    */
+    
     private static class RemoveFinalModifierUnsupported implements RemoveFinalModifierMethod {
         @Override
         public void remove(Field field) throws IllegalAccessException {
             throw new IllegalAccessException("Changing the value of final fields is not supported on this JVM");
         }
-
-        @Override
-        public boolean valid() {
-            return false;
-        }
     }
 
     private static interface RemoveFinalModifierMethod {
         void remove(java.lang.reflect.Field field) throws IllegalAccessException;
-        boolean valid();
     }
 }
